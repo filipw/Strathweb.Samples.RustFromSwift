8a22a24d261f7ea42f1b9afc109ff0808e438ccf
filipw <wojcieszyn@gmail.com>
2023-07-25 21:49:49 +0200

    clean up

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ NAME="rust_lib"
 STATIC_LIB_NAME="lib${NAME}.a"
 NEW_HEADER_DIR="bindings/include"
 
-mkdir -p "${NEW_HEADER_DIR}"
-cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
-cp "bindings/rust_libFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
-
 cargo build --target aarch64-apple-darwin --release
 cargo build --target aarch64-apple-ios --release
 cargo build --target aarch64-apple-ios-sim --release
+
+mkdir -p "${NEW_HEADER_DIR}"
+cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
+cp "bindings/rust_libFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
 
 rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
 
